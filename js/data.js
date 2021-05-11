0771a9a36ace0396c07143ae9dd76e9aff49e728
p0pkern <45568242+p0pkern@users.noreply.github.com>
2021-05-10 19:53:00 -0700

    initial commit

---
+++ js/data.js
@@ -0,0 +1,7 @@
+class StartPlayer {
+    constructor() {
+        this.larva = 0;
+        this.harvest = 0;
+        this.meat = 0;
+    }
+}
---
+++ js/script.js
@@ -0,0 +1,22 @@
+let player = new StartPlayer
+
+function updateStats() {
+    document.getElementById('main-stats').innerText =
+    `Larva: ${player.larva} harvest: ${player.harvest} Meat: ${player.meat}`
+}
+
+function mineLarva() {
+    player.larva += 1;
+    updateStats()
+}
+
+function mineHarvest() {
+    player.harvest += 1;
+    updateStats()
+}
+
+function mineMeat() {
+    player.meat += 1;
+    updateStats()
+}
+
